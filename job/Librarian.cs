6512ac6858cd538e421c652cb58bf04629f280cf
unknown <rcisneros138@gmail.com>
2015-11-23 16:05:58 -0600

    first commit

---
+++ job/Librarian.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace job
+{
+    class Librarian
+    {
+        public string title;
+        public string author;
+        public string genre;
+        public void inputBookInfo()
+        {
+            Console.Write("Enter book title: ");
+            title = Console.ReadLine();
+            Console.Write("Enter book Author: ");
+            author = Console.ReadLine();
+            Console.Write("Enter book genre: ");
+            genre = Console.ReadLine();
+
+        }
+    }
+        
+}
---
+++ job/Storage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace job
+{
+    class Storage
+    {
+        Organization organization = new Organization();
+   
+        public void storeBook()
+        {
+            //put in correct location.
+        }  
+    }
+}
